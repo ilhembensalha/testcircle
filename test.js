0da4f6e6ca3288d3663deb69a99507921c9af84b
ilhembensalha <bensalhailhem@gmail.com>
2022-12-11 23:39:22 +0100

    test4

--- test.js
+++ test.js
@@ -1,4 +1,28 @@
-test('test 1', async (t) => {
-    await t.test('test 2');
-    await t.test('test 3');
-  });
+describe('SearchEngine', () => {
+
+    it('should pass locale to third party api', () => {
+
+        /* Spying on `thirdPartySearchApi.search` and faking result. */
+        spyOn(thirdPartySearchApi, 'search').and.returnValue([
+            {
+                title: 'Wishtack - Making Your Wishes Come True',
+                url: 'https://www.wishtack.com'
+            }
+        ]);
+
+        /* Trigger search. */
+        searchEngine.search({keywords: 'Wishtack'});
+
+        /* Check spy's call count. */
+        expect(thirdPartySearchApi.search.callCount).toBe(1);
+
+        /* Check spy's call args. */
+        expect(thirdPartySearchApi.search).toHaveBeenCalledWith({
+            country: 'US',
+            keywords: 'Wishtack',
+            language: 'en'
+        });
+
+    });
+
+});
