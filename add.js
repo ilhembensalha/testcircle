1d73c15b19c9b2e5318c6fc179579b28f633d581
ilhembensalha <bensalhailhem@gmail.com>
2022-12-12 00:31:28 +0100

    test7final

--- add.js
+++
@@ -1,6 +0,0 @@
-let a = 2
-let b = 2
-
-function add(a, b) {
-    return a + b
-  }
--- add.test.js
+++
@@ -1,4 +0,0 @@
-add("2 + 2 should be equal to 4", function() {
-    var a = 2 + 2;
-    expect(a).toEqual(4);
-  });
